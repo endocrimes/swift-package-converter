1077c94fdc94bf0e15fa18fa6af2bec77a9b961c
Honza Dvorsky <jan.dvorsky@yahoo.com>
2016-07-15 15:03:19 +0200

    new tasks

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "swift-package-converter",
     dependencies: [
     	.Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 12),
-    	.Package(url: "https://github.com/czechboy0/Tasks.git", majorVersion: 0, minor: 1),
+    	.Package(url: "https://github.com/czechboy0/Tasks.git", majorVersion: 0, minor: 2),
     	.Package(url: "https://github.com/czechboy0/Environment.git", majorVersion: 0, minor: 4),
     ]
 )
